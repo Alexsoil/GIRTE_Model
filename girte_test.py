1f57993b4672d23d47dc831b15ccea1bc73964ab
alexsoil <alexsoilgr@gmail.com>
2024-05-23 14:40:19 +0300

    Updated GIRTE class and testing file, to be restructured

--- girte_test.py
+++ girte_test.py
@@ -1,7 +1,8 @@
 import sys
 import os
 import pickle
-import networkx
+from networkx import edges
+from numpy import mean
 from tqdm import tqdm
 from models.GIRTE import GIRTEModel
 from utilities.Result_handling import expir_start
@@ -14,11 +15,21 @@ try:
     col_path = 'experiments/collections/CF'
     dest_path = 'experiments/paper_results'
 
-    test_collection, relevant, queries = expir_start(path_documents, path_to_write, col_path)
+    test_collection, queries, relevant  = expir_start(path_documents, path_to_write, col_path)
 
-    # print(test_collection.docs[0].terms)
-    abnaroz = GIRTEModel(collection=test_collection, theta=0.7, save=True)
-    print(abnaroz.graph)
+    # with open(os.path.join(os.getcwd(), 'experiments/temp/GIRTE_CF_07.model'), 'rb') as picklefile:
+    #     abnaroz = pickle.load(picklefile)
+
+    N = GIRTEModel(test_collection, 0.7)
+    for item in N.collection.inverted_index.items():
+        print(item)
+    # for k in N.collection.inverted_index:
+    #     nwk_temp = type(k)
+    #     print(f'{k} -> {nwk_temp}')
+    # abnaroz.fit(min_freq=10)
+    # abnaroz.evaluate()
+    # print(mean(abnaroz.precision))
+    # print(abnaroz.graph)
     print('Done')
 except Exception as err:
     print(f'Door Stuck. {err}')
