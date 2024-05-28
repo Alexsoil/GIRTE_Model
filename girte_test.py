0845a55da3fa5880edf3e94a7377f1842d4e0439
alexsoil <alexsoilgr@gmail.com>
2024-05-28 18:51:23 +0300

    Successful run of test collection and save

--- girte_test.py
+++ girte_test.py
@@ -1,10 +1,19 @@
 from Preprocess.Tok_Document import TokDocument
-
+from utilities.Result_handling import expir_start
+from pickle import dump
+import os
 
 try:
-    test_path = 'experiments/collections/CF/docs/00001'
-    T = TokDocument(test_path)
-    print(type(T.tokens[0]))
-    print('Done')
+    #CF
+    path = 'experiments/collections/CF/docs'
+    path_to_write = 'experiments/temp'
+    col_path = 'experiments/collections/CF'
+    dest_path = "experiments/paper_results"
+
+    testcollection, q, r = expir_start(path, path_to_write, col_path, token=True)
+    os.makedirs('picklejar/test')
+    with open('picklejar/test/col.pickle', 'wb') as picklefile:
+        tup = (testcollection, q, r)
+        dump(tup, picklefile)
 except Exception as err:
     print(f'Door Stuck. {err}')
