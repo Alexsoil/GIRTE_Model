67d64b28e4d539e9befcf2a657d63ed525c41ab6
alexsoil <alexsoilgr@gmail.com>
2024-05-30 18:00:45 +0300

    Implemented GSB with BERT tokens as GIRTE. Needs testing.

--- girte_test.py
+++ girte_test.py
@@ -1,6 +1,7 @@
 from Preprocess.Tok_Document import TokDocument
+from models.GIRTE import GIRTEModel
 from utilities.Result_handling import expir_start
-from pickle import dump
+from pickle import dump, load
 import os
 
 try:
@@ -9,11 +10,26 @@ try:
     path_to_write = 'experiments/temp'
     col_path = 'experiments/collections/CF'
     dest_path = "experiments/paper_results"
+    save_path = 'picklejar/test/col.pickle'
 
-    testcollection, q, r = expir_start(path, path_to_write, col_path, token=True)
-    os.makedirs('picklejar/test')
-    with open('picklejar/test/col.pickle', 'wb') as picklefile:
-        tup = (testcollection, q, r)
-        dump(tup, picklefile)
+    try:
+        with open(save_path, 'rb') as picklefile:
+            cqr = load(picklefile)
+        testcollection = cqr[0]
+        q = cqr[1]
+        r = cqr[2]
+    except FileNotFoundError:
+        print('Saved file not found. Re-creating test collection.')
+        testcollection, q, r = expir_start(path, path_to_write, col_path, token=True)
+        os.makedirs('picklejar/test')
+        with open(save_path, 'wb') as picklefile:
+            tup = (testcollection, q, r)
+            dump(tup, picklefile)
+    # Test Start
+    N = GIRTEModel(testcollection)
+    N.fit(min_freq=10)
+    N.evaluate()
+    print(N.precision)
+    print('Done')
 except Exception as err:
     print(f'Door Stuck. {err}')
--- models/GIRTE.py
+++ models/GIRTE.py
@@ -1,168 +1,74 @@
-import sys
-import os
 from models.GSB import GSBModel
-from models.Model import Model
-from networkx import Graph, compose
-from transformers import BertTokenizer, BertModel
-import torch
-import time
-import random
-import itertools
-import pickle
+from networkx import Graph, set_node_attributes
+from numpy import array, dot, fill_diagonal
+from Preprocess import Tok_Document, Tok_Collection
+from transformers import BertTokenizer
+from utilities.document_utls import calc_average_edge_w, prune_matrix, adj_to_graph, nodes_to_terms
+from utilities.apriori import apriori
 from tqdm import tqdm
-from sklearn.metrics.pairwise import cosine_similarity
-from Preprocess import Document, Collection
 
 class GIRTEModel(GSBModel):
-    def __init__(self, collection, theta, k_core_bool=False, h_val=1, p_val=0, save=False):
-        self.theta = float(theta)
+    def __init__(self, collection: Tok_Collection, k_core_bool=False, h_val=1, p_val=0):
         super().__init__(collection, k_core_bool, h_val, p_val)
-        if save:
-            # Saving as pickle data
-            with open(os.path.join(os.getcwd(), 'experiments/temp/GIRTE_CF_07.m'), 'wb') as picklefile:
-                pickle.dump(self, picklefile)
-            
-        
-    # def union_graph(self):
-    #     G = Graph()
-    #     # Total timer start
-    #     total_t_start = time.time()
-    #     if __debug__:
-    #         max_iterations = 1
-    #     elif int(sys.argv[1]) == 0 or int(sys.argv[1]) > self.collection.num_docs:
-    #         max_iterations = self.collection.num_docs
-    #     else:
-    #         max_iterations = int(sys.argv[1])
-
-    #     # For each document...
-    #     iterations = 0
-    #     # Seed for multithreading
-    #     random_seed = 69
-    #     random.seed(random_seed)
-    #     if torch.cuda.is_available():
-    #             torch.cuda.manual_seed_all(random_seed)
-            
-    #     # Loading pretrained Bert Models
-    #     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    #     model = BertModel.from_pretrained('bert-base-uncased')
-
-    #     # Encoding + Embedding timer start
-    #     for doc in tqdm(self.collection.docs):
-    #         bert_t_start = time.time()
-    #         # Encode document words into tokens
-    #         encoding = tokenizer.__call__(
-    #             doc.terms,
-    #             padding=True,
-    #             truncation=True,
-    #             return_tensors='pt',
-    #             add_special_tokens=True,
-    #             is_split_into_words=True
-    #         )
-    #         if __debug__:
-    #             print('Encoding complete.')
-
-    #         input_ids = encoding['input_ids']
-    #         attention_mask = encoding['attention_mask']
-    #         if __debug__:
-    #             print(f'Input ID: {input_ids}')
-    #             print(f'Attention mask: {attention_mask}')
-
-    #         # Get last layer of BERT as tensors
-    #         with torch.no_grad():
-    #             outputs = model(input_ids, attention_mask=attention_mask)
-    #             word_embeddings = outputs.last_hidden_state
-    #         if __debug__:
-    #             print('Embeddings created.')
-    #         decoded_text = tokenizer.decode(input_ids[0], skip_special_tokens=True)
-    #         tokenized_text = tokenizer.tokenize(decoded_text)
-    #         encoded_text = tokenizer.encode(' '.join(doc.terms), return_tensors='pt')
-    #         if __debug__:
-    #             print(f'Decoded Text: {decoded_text}')
-    #             print(f'Tokenized Text: {tokenized_text}')
-    #             print(f'Encoded Text: {encoded_text}')
-    #             print(word_embeddings.shape)
-    #         # Encoding + Embedding timer end
-    #         bert_t_end = time.time()
-
-    #         document_graph = Graph()
-    #         # Graph time start
-    #         graph_t_start = time.time()
-            
-    #         # Choose the next word
-    #         for tok, i in zip(tokenized_text, word_embeddings[0]):
-                
-    #             # if not in the graph, hold it
-    #             if document_graph.has_node(tok) is False:
-    #                 # iterate through the rest of the words
-    #                 embedding_combiner = []
-    #                 for tok_comp, j in zip(tokenized_text, word_embeddings[0]):
-    #                     # if any word is equal to the one held, append them to a list
-    #                     if tok_comp == tok:
-    #                         # print('match')
-    #                         embedding_combiner.append(j)
-    #                 # when every word has been checked, take the mean of the embeddings for held word
-    #                 stacked_embedding = torch.stack(embedding_combiner)
-    #                 # Shape: [X, 768]
-    #                 mean_embedding = torch.mean(stacked_embedding, dim=0)
-    #                 # Shape: [768]w
-    #                 document_graph.add_node(tok, tensor=mean_embedding)
-    #             else:
-    #                 # if it already is in the graph, skip it
-    #                 continue
     
-    #         # cache reshaped nodes
-    #         reshaped_nodes = {}
-    #         for node in document_graph.nodes(data='tensor'):
-    #             reshaped_nodes[node[0]] = torch.reshape(node[1], (1, -1))
+    def doc_to_matrix(self, document: Tok_Document):
+        rows = array(list(document.token_frequency.values()))
+        row = rows.reshape(1, rows.shape[0]).T
+        col = rows.reshape(rows.shape[0], 1).T
+        adj_matrix = dot(row, col)
+        win = [(w * (w + 1) * 0.5) for w in rows]
+        fill_diagonal(adj_matrix, win)
+        return adj_matrix
 
-    #         # Calculate cosine similarity and add edges
-    #         for node_outer, node_inner in itertools.combinations(document_graph.nodes(data='tensor'), 2):
-    #             similarity = cosine_similarity(reshaped_nodes[node_outer[0]], reshaped_nodes[node_inner[0]])
-    #             if similarity[0][0] < 1 - self.theta:
-    #                 document_graph.add_edge(node_outer[0], node_inner[0], weight=similarity[0][0])
-
-    #         # Calculate cosine similarity for each node with itself
-    #         for node in document_graph.nodes(data='tensor'):
-    #             similarity = cosine_similarity(reshaped_nodes[node[0]], reshaped_nodes[node[0]])
-    #             if similarity[0][0] < 1 - self.theta:
-    #                 document_graph.add_edge(node[0], node[0], weight=similarity[0][0])
+    def union_graph(self):
+        union = Graph()
+        for doc in self.collection.docs:
+            tokens = list(doc.token_frequency.keys())
+            adj_matrix = self.doc_to_matrix(doc)
+            kcore = []
+            if self.k_core_bool:
+                if self.model == "GSBModel":
+                    thres_edge_weight = self.p * calc_average_edge_w(adj_matrix)
+                    adj_matrix = prune_matrix(adj_matrix, thres_edge_weight)
+                    g = adj_to_graph(adj_matrix)
+                    maincore = self.kcore_nodes(g)
+                    kcore = nodes_to_terms(tokens, maincore)
             
-    #         # Graph timer end
-    #         graph_t_end = time.time()
-
-    #         if __debug__:
-    #             # Print nodes
-    #             print('Node -> 1st value of Tensor')
-    #             for node in document_graph.nodes(data='tensor'):
-    #                 print(f'{node[0]} -> {node[1][0]}')
-
-    #             # Print edges
-    #             print('Edge -> Weight')
-    #             for edge in document_graph.edges(data='weight'):
-    #                 print(f'{edge[0]} -> {edge[1]} ({edge[2]})')
-
-    #         # Graph Information
-    #         num_nodes = document_graph.number_of_nodes()
-    #         num_edges = document_graph.number_of_edges()
-    #         G = compose(G, document_graph)
-    #         print(f'Graph with {num_nodes} nodes, {num_edges}/{int((num_nodes * (num_nodes - 1)) / 2) + num_nodes} possible edges.')
-    #         print(f'Iteration total time: {(graph_t_end - total_t_start):.2f}\tEmbedding: {(bert_t_end - bert_t_start):.2f}\tGraph: {(graph_t_end - graph_t_start):.2f}')
-        
-    #         # if not __debug__:
-    #         #     # Saving as pickle data
-    #         #     os.makedirs(os.path.join(model_location), exist_ok=True)
-    #         #     with open(os.path.join(model_location, filename + '.graph'), 'wb') as picklefile:
-    #         #         pickle.dump(G, picklefile)
-
-    #         # with open(os.path.join(model_location, filename), 'rb') as pickfile:
-    #         #     abnaroz = pickle.load(pickfile)
-    #         # print(abnaroz.number_of_nodes())
-    #         iterations += 1
-    #         if iterations >= max_iterations:
-    #             break
-    #     # Total timer end
-    #     total_t_end = time.time()
-    #     print(f'Processed {iterations} documents.\tTotal time: {(total_t_end - total_t_start)/60:.2f} minutes')
-    #     return G
-
+            for i in range(adj_matrix.shape[0]):
+                h = self.h if tokens[i] in kcore and self.k_core_bool else 1
+                for j in range(adj_matrix.shape[1]):
+                    if i >= j:
+                        if union.has_edge(tokens[i], tokens[j]):
+                            union[tokens[i]][tokens[j]]['weight'] += (adj_matrix[i][j] * h)
+                        else:
+                            if adj_matrix[i][j] > 0:
+                                union.add_edge(tokens[i], tokens[j], weight=adj_matrix[i][j])
+        w_in = {n: union.get_edge_data(n, n)['weight'] for n in union.nodes()}
+        set_node_attributes(union, w_in, 'weight')
+        for n in union.nodes: union.remove_edge(n, n)
+        return union
     
+    def fit(self, term_queries=None, min_freq=1):
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        if term_queries is None:
+            term_queries = self._queries
+        token_queries = []
+        for term_query in tqdm(term_queries):
+            encoding = tokenizer.__call__(
+                term_query,
+                padding=True,
+                truncation=True,
+                add_special_tokens=True,
+                is_split_into_words=True
+                )
+            tokenized_query = tokenizer.convert_ids_to_tokens(encoding['input_ids'], skip_special_tokens=True)
+            token_queries.append(tokenized_query)
+        inverted_index = self.collection.inverted_index
+        print(f'Processing {len(token_queries)} Queries')
+        for i, query in enumerate(token_queries):
+            print(f'Q{i}: (Len = {len(query)}) {' '.join(query)}')
+            freq_termsets = apriori(query, inverted_index, min_freq)
+            self._queryVectors.append(self.calculate_ts_idf(freq_termsets))
+            self._docVectors.append(self.calculate_tsf(freq_termsets))
+            self._weights.append(self._model_func(freq_termsets))
+        return self
