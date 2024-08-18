1980047596aa4e236fc24bd9a1df69a491a089f5
Alexsoil <alexsoilgr@gmail.com>
2024-08-18 18:33:15 +0300

    Updated to work with complete GIRTE implementation, introduced arguements for bert size and document stopwords

--- girte_test.py
+++ girte_test.py
@@ -6,50 +6,50 @@ from numpy import mean
 import os
 from time import time
 
-def testing(minimum_frequency, trimmed_docs, use_stopwords=True):
-    try:
+def testing(minimum_frequency, tensors=False, bert='base', d_stopwords=False, q_stopwords=True):
+    # try:
         #CF
-        path = 'experiments/collections/CF/docs'
-        path_to_write = 'experiments/temp'
-        col_path = 'experiments/collections/CF'
-        dest_path = "experiments/paper_results"
+    path = os.path.join('experiments', 'collections', 'CF', 'docs')
+    path_to_write = os.path.join('experiment', 'temp')
+    col_path = os.path.join('experiments', 'collections', 'CF')
+    dest_path = os.path.join('experiments', 'paper_results')
 
-        if trimmed_docs == True:
-            save_path = 'picklejar/test/col2.pickle'
-        else:
-            save_path = 'picklejar/test/col.pickle'
+    bert_type = 'large' if bert == 'large' else 'base'
+    if d_stopwords == True:
+        save_path = f'C:/picklejar/collections/col.{bert_type}.sw'
+    else:
+        save_path = f'C:/picklejar/collections/col.{bert_type}.nsw'
 
-        try:
-            with open(save_path, 'rb') as picklefile:
-                cqr = load(picklefile)
-            testcollection = cqr[0]
-            q = cqr[1]
-            r = cqr[2]
-        except FileNotFoundError:
-            print('Saved file not found. Re-creating test collection.')
-            testcollection, q, r = expir_start(path, path_to_write, col_path, token=True)
-            os.makedirs('picklejar/test', exist_ok=True)
-            with open(save_path, 'wb') as picklefile:
-                tup = (testcollection, q, r)
-                dump(tup, picklefile)
-        
-        # Test Start
-        N = GIRTEModel(testcollection)
-        fit_start = time()
-        N.fit(min_freq=minimum_frequency, use_stopwords=use_stopwords)
-        fit_end = time()
-        N.evaluate()
-        prec = mean(N.precision)
-        timer = fit_end - fit_start
-        print(f'Mean Precision: {prec:.2f}')
-        print(f'Total time: {timer:.2f}')
-        print(N._nwk)
-        return prec, timer
+    try:
+        with open(save_path, 'rb') as picklefile:
+            cqr = load(picklefile)
+        testcollection = cqr[0]
+        q = cqr[1]
+        r = cqr[2]
+    except FileNotFoundError:
+        print('Saved file not found. Re-creating test collection.')
+        testcollection, q, r = expir_start(path, path_to_write, col_path, token=True, bert=bert_type, stopwords=d_stopwords)
+        os.makedirs('C:/picklejar', exist_ok=True)
+        with open(save_path, 'wb') as picklefile:
+            tup = (testcollection, q, r)
+            dump(tup, picklefile)
+    
+    # Test Start
+    N = GIRTEModel(testcollection, tensors=tensors, bert=bert_type, stopwords=d_stopwords, theta_val=0.69)
+    fit_start = time()
+    N.fit(min_freq=minimum_frequency, use_stopwords=q_stopwords)
+    fit_end = time()
+    N.evaluate()
+    prec = mean(N.precision)
+    timer = fit_end - fit_start
+    print(f'Mean Precision: {prec:.2f}')
+    print(f'Total time: {timer:.2f}')
+    return prec, timer
 
-    except Exception as err:
-        print(f'Door Stuck. {err}')
+    # except Exception as err:
+    #     print(f'Door Stuck. {err}')
 
 output = []
-precision, time_to_complete = testing(16, False, True)
-output.append(f'Minimum Frequency: {16} Precision: {precision:.4f} Time: {time_to_complete:.2f} seconds.')
+precision, time_to_complete = testing(10, tensors=True, bert='base', d_stopwords=False, q_stopwords=True)
+output.append(f'Minimum Frequency: {10} Precision: {precision:.4f} Time: {time_to_complete:.2f} seconds.')
 print(*output, sep='\n')
