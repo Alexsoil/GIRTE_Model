faba37f2f34a96867bf55f7a6d54f3ddeb9cab68
Alexsoil <alexsoilgr@gmail.com>
2024-08-18 18:29:33 +0300

    Introduced save and load features for time-consuming tasks, implemented the use of tensors alongside the GSB model, implemented theta.

--- models/GIRTE.py
+++ models/GIRTE.py
@@ -1,7 +1,9 @@
 from models.GSB import GSBModel
+from models.Model import Model
 from networkx import Graph, set_node_attributes
-from numpy import array, dot, fill_diagonal
-from Preprocess import Tok_Document, Tok_Collection
+from numpy import array, dot, fill_diagonal, zeros
+from Preprocess.Tok_Document import TokDocument
+from Preprocess.Tok_Collection import TokCollection
 from transformers import BertTokenizer
 from utilities.document_utls import calc_average_edge_w, prune_matrix, adj_to_graph, nodes_to_terms
 from utilities.apriori import apriori
@@ -9,51 +11,95 @@ from sklearn.metrics.pairwise import cosine_similarity
 from tqdm import tqdm
 from time import time
 from nltk.corpus import stopwords
+from pickle import load, dump
+import torch
+from os import makedirs
+
+# Path : default_path +
+default_path = 'C:/picklejar'
 
 class GIRTEModel(GSBModel):
-    def __init__(self, collection: Tok_Collection, k_core_bool=False, h_val=1, p_val=0):
-        super().__init__(collection, k_core_bool, h_val, p_val)
-    
-    def doc_to_matrix(self, document: Tok_Document):
-        rows = array(list(document.token_frequency.values()))
-        row = rows.reshape(1, rows.shape[0]).T
-        col = rows.reshape(rows.shape[0], 1).T
-        adj_matrix = dot(row, col)
-        win = [(w * (w + 1) * 0.5) for w in rows]
-        fill_diagonal(adj_matrix, win)
-        return adj_matrix
+    def __init__(self, collection: TokCollection, tensors=False, bert='base', stopwords=False, theta_val=0, k_core_bool=False, h_val=1, p_val=0):
+        self.start_time = time()
+        self.k_core_bool = k_core_bool
+        if isinstance(h_val, int):
+            self.h = h_val
+        elif isinstance(h_val, float):
+            self.h = h_val * 100
+        if isinstance(p_val, int):
+            self.p = p_val / 100
+        elif isinstance(p_val, float):
+            self.p = p_val
+        Model.__init__(self, collection)
+        self._default_path = default_path
+        # False: TF-IDF | True: Cosine similarity
+        self._method = 'ts' if tensors else 'tf'
+        # bert-base-uncased | bert-large-uncased
+        self._bert = 'large' if bert == 'large' else 'base'
+        # False: No document stopwords | True: Document stopwords
+        self._stopwords = 'sw' if stopwords else 'nsw'
+
+        self.model = self.get_model()
+        if self._method == 'tf':
+            self.graph = self.union_graph()
+        elif self._method == 'ts':
+            self.graph = self.union_graph_tensor(theta=theta_val)
+
+        self._nwk = self._calculate_nwk()
+        self.end_time = time()
+        print(f'Model took {self.end_time - self.start_time} seconds')
 
     def union_graph(self):
         union = Graph()
-        for doc in self.collection.docs:
-            tokens = doc.tokens
-            # tensors = doc.tensors
-            adj_matrix = self.doc_to_matrix(doc)
+        matrice_dictionary = self._load_matrices()
+        print('Building union graph...')
+        for document in tqdm(self.collection.docs):
+            tokens = list(document.token_frequency.keys())
+            adj_matrix = matrice_dictionary[document.doc_id]
             kcore = []
-            if self.k_core_bool:
-                if self.model == "GSBModel":
-                    thres_edge_weight = self.p * calc_average_edge_w(adj_matrix)
-                    adj_matrix = prune_matrix(adj_matrix, thres_edge_weight)
-                    g = adj_to_graph(adj_matrix)
-                    maincore = self.kcore_nodes(g)
-                    kcore = nodes_to_terms(tokens, maincore)
-            
+            # Iterate over rows
             for i in range(adj_matrix.shape[0]):
                 h = self.h if tokens[i] in kcore and self.k_core_bool else 1
+                # Iterate over columns
                 for j in range(adj_matrix.shape[1]):
                     if i >= j:
                         if union.has_edge(tokens[i], tokens[j]):
-                            union[tokens[i]][tokens[j]]['weight'] += (adj_matrix[i][j] * h)
+                            union[tokens[i]][tokens[j]]['weight'] += (adj_matrix[i, j] * h)
                         else:
                             if adj_matrix[i][j] > 0:
-                                union.add_edge(tokens[i], tokens[j], weight=adj_matrix[i][j])
+                                union.add_edge(tokens[i], tokens[j], weight=adj_matrix[i, j])
         w_in = {n: union.get_edge_data(n, n)['weight'] for n in union.nodes()}
         set_node_attributes(union, w_in, 'weight')
         for n in union.nodes: union.remove_edge(n, n)
         return union
     
+    def union_graph_tensor(self, theta=0):
+        union = Graph()
+        matrice_dictionary = self._load_matrices()
+        print('Building union graph...')
+        for document in tqdm(self.collection.docs):
+            tokens = list(document.token_frequency.keys())
+            adj_matrix = matrice_dictionary[document.doc_id]
+            kcore = []
+            # Iterate over rows
+            for i in range(adj_matrix.shape[0]):
+                h = self.h if tokens[i] in kcore and self.k_core_bool else 1
+                # Iterate over columns
+                for j in range(adj_matrix.shape[1]):
+                    if i >= j:
+                        if union.has_edge(tokens[i], tokens[j]):
+                            union[tokens[i]][tokens[j]]['weight'] += (adj_matrix[i, j] * h)
+                        else:
+                            if adj_matrix[i][j] > theta:
+                                union.add_edge(tokens[i], tokens[j], weight=adj_matrix[i, j])
+        w_in = {n: union.get_edge_data(n, n)['weight'] for n in union.nodes()}
+        set_node_attributes(union, w_in, 'weight')
+        # Delete Self-Edges
+        for n in union.nodes: union.remove_edge(n, n)
+        return union
+
     def fit(self, term_queries=None, min_freq=1, use_stopwords=True):
-        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        tokenizer = BertTokenizer.from_pretrained(f'bert-{self._bert}-uncased')
         if term_queries is None:
             term_queries = self._queries
         token_queries = []
@@ -89,3 +135,50 @@ class GIRTEModel(GSBModel):
             self._weights.append(self._model_func(freq_termsets))
             print(f'Q{i} Apriori: {(qvectors_start - apriori_start):.2f}\tQvectors: {(dvectors_start - qvectors_start):.2f}\tDvectors: {(time_end - dvectors_start):.2f}\tTotal: {(time_end-apriori_start):.2f}')
         return self
+
+    def _load_matrices(self) -> dict[int, array]:
+        load_path = f'C:/picklejar/matrices/mat.{self._bert}.{self._stopwords}.{self._method}'
+        try:
+            # Load matrices from disk, if it already exists.
+            with open(load_path, 'rb') as picklefile:
+                matrix_dictionary = load(picklefile)
+            print('Matrix dictionary loaded from disk.')
+        except:
+            # If the file doesn't exist, genereate the matrices and save for future use. 
+            print('Matrix dictionary not found, rebuilding...')
+            matrix_dictionary = {} # {Document: Matrix}
+            if self._method == 'tf':
+                for document in tqdm(self.collection.docs):
+                    matrix_dictionary[document.doc_id] = self._doc_to_matrix(document)
+            elif self._method == 'ts':
+                tensor_path = f'C:/picklejar/tensors/{self._bert}/{self._stopwords}/'
+                for document in tqdm(self.collection.docs):
+                    with open(tensor_path + str(document.doc_id), 'rb') as picklefile:
+                        document_tensors = load(picklefile)
+                    matrix_dictionary[document.doc_id] = self._doc_to_matrix_tensor(document_tensors)
+            makedirs('C:/picklejar/matrices', exist_ok=True)
+            with open(load_path, 'wb') as picklefile:
+                dump(matrix_dictionary, picklefile)
+            print('Saved Matrix dictionary on disk.')
+        return matrix_dictionary
+    
+    def _doc_to_matrix(self, document: TokDocument) -> array:
+    #Generate adjacency matrix of a given document based on token frequency.
+        rows = array(list(document.token_frequency.values()))
+        row = rows.reshape(1, rows.shape[0]).T
+        col = rows.reshape(rows.shape[0], 1).T
+        adj_matrix = dot(row, col)
+        win = [(w * (w + 1) * 0.5) for w in rows]
+        fill_diagonal(adj_matrix, win)
+        return adj_matrix
+    
+    def _doc_to_matrix_tensor(self, tensors: dict[str, torch.tensor]) -> array:
+    #Generate adjacency matrix of a given document based on cosine similarity.
+        adj_matrix = zeros((len(tensors), len(tensors)))
+        for i, (i_key, i_value) in enumerate(tensors.items()):
+            for j, (j_key, j_value) in enumerate(tensors.items()):
+                if i == j:
+                    adj_matrix[i, j] = 1
+                else:
+                    adj_matrix[i, j] = cosine_similarity(torch.reshape(i_value, (1, -1)), torch.reshape(j_value, (1, -1)))[0][0]
+        return adj_matrix
